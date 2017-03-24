37b2a69a1a8a31f5bf2d4b1082fc858631610661
Jonathan Dietz <jonathanmd@gmail.com>
2017-03-24 15:56:58 -0700

    output list of used Availability Zones

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,14 @@ output "vpc_id" {
   value = "${aws_vpc.the_vpc.id}"
 }
 
+# Returns the availability zones used by vpc
+output "availability_zones" {
+  # value = ["${data.aws_availability_zones.available.names}"]
+  value = ["${slice(data.aws_availability_zones.available.names, 0, 
+  		(var.max_zones > length(data.aws_availability_zones.available.names) ? 
+        length(data.aws_availability_zones.available.names):var.max_zones))}"]
+}
+
 # Return Internet gateway ID
 output "igw_id" {
   value = "${aws_internet_gateway.primary_igw.id}"
